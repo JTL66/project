38abfe10917a3ce6dd2dbbe84f17a00051e9ab2e
JTL66 <65905010+JTL66@users.noreply.github.com>
2020-11-28 11:26:31 -0500

    commit

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
-# Please have these command installed and import 
+"""
+This project it jupyter notebook based. Open with Github Desktop and open in 
+Visual Studio Code. 
+"""
+"""
+#Some installments
+pip install pandas
+pip intsall requests
+pip install matplotlib
+pip install seaborn
+pip install nba_api
+"""
+# Please have these commands installed and import 
 import pandas as pd
 import numpy as np
 import requests
-import matplotlib.pyplot as plt
 import os
 import json
-import seaborn as sns 
+import seaborn as sns
+from urllib.request import urlopen
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from nba_api.stats.endpoints import shotchartdetail 
 
 ## Read data
-
 data = pd.read_csv("nbashot19.csv")
 data.head() #head the first 5 rows
 
