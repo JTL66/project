ffc567f017a6a13c57a29ba776a39de58c31ea19
JTL66 <65905010+JTL66@users.noreply.github.com>
2020-12-01 15:38:33 -0500

    commit

--- setup.py
+++ setup.py
@@ -23,21 +23,15 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from nba_api.stats.endpoints import shotchartdetail 
 
-## Read data
-data = pd.read_csv("nbashot19.csv")
-data.head() #head the first 5 rows
-
-## non-missing value
-data =  data[data['SHOT_MADE_FLAG'].notnull()]
-
 ## Select Curry's data 
-Curry_data = data[data['PLAYER_NAME'] == 'Stephen Curry']
+Curry_data = pd.read_csv("curry.csv")
 Curry_data.head()
+## non-missing value
+Curry_data =  curry_data[curry_data['SHOT_MADE_FLAG'].notnull()]
+
 
 ## Select Giannis's data
-Giannis_data = data[data['PLAYER_NAME'] == 'Giannis Antetokounmpo']
+Giannis_data = pd.read_csv("giannis.csv")
 Giannis_data.head()
-
-## Select Simmons's data
-Simmons_data = data[data['PLAYER_NAME'] == 'Ben Simmons']
-Simmons_data.head()
+## non-missing value
+Giannis_data = Giannis_data[Giannis_data['SHOT_MADE_FLAG'].notnull()]
